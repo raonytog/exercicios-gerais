e966c949c77a481eb0e934f7eed541df30e55cb6
raonytog <togneriraony@gmail.com>
2023-08-24 18:02:17 -0300

    Atualizações no rev4 (usei para escrever o redme)

--- 01_revisao/rev_04/rev4.c
+++ 01_revisao/rev_04/rev4.c
@@ -1,7 +1,26 @@
-#include <stdio.h>
+// usei esse código para fazer/copiar o html do read.me do profile do github 
 
-int main () {
-    //teste
-    printf("Teste! C:);
-    return 0;
-}
+<div align="center">
+	<h1>Olá!</h1>
+	<picture>
+  <source
+    srcset="https://github-readme-stats.vercel.app/api?username=raonytog&show_icons=true&theme=dark"
+    media="(prefers-color-scheme: dark)"
+  />
+  <source
+    srcset="https://github-readme-stats.vercel.app/api?username=raonytog&show_icons=true"
+    media="(prefers-color-scheme: light), (prefers-color-scheme: no-preference)"
+  />
+  <img src="https://github-readme-stats.vercel.app/api?username=raonytog&show_icons=true" />
+</picture>
+<p>
+	- 🎈 Estudante de Ciência da Computaão na UFES;
+	- 👾Aprendendo C;
+</p>
+</div>
+
+
+
+
+
+
