1a70fd53281ec7d2a4dfef66b865f3c789e297f8
raonytog <togneriraony@gmail.com>
2023-08-24 23:21:21 -0300

    rec02 resolvido

---
+++ 02_recursao/rec_02/rec2.c
@@ -0,0 +1,43 @@
+/*
+Programa que soma todos os números pares (n%2==0) de uma lista de arrays a partir da qtd de vetores,
+o tamanho de cada um, seguido do conteudo dos vetores.
+Raony Togneri
+24/08/2023
+*/
+
+#include <stdio.h>
+
+int SomaElementosPares(int* vet, int numElementos);
+void PreencheVetor(int tam, int vet[tam]);
+
+int main () {
+    int numVetores = 0, tam = 0;
+    scanf("%d", &numVetores);
+    for(int i = 0; i < numVetores; i++) {
+        scanf("%d", &tam);
+        int vet[tam];
+        PreencheVetor(tam, vet);
+        printf("%d\n", SomaElementosPares(vet, tam));
+    }
+    return 0;
+}
+
+void PreencheVetor(int tam, int vet[tam]) {
+    for(int i = 0; i < tam; i++) {
+        scanf("%d%*c", &vet[i]);
+    }
+}
+
+int SomaElementosPares(int* vet, int numElementos) {
+    int i = 0;
+    numElementos--;
+    if (numElementos == -1) {
+        return 0;
+
+    } else if (vet[numElementos]%2 == 0) {
+        return vet[numElementos] + SomaElementosPares(vet, numElementos);
+    } else {
+        return SomaElementosPares(vet, numElementos);
+    }
+
+}
