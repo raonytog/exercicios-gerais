0ec09009514656160fa70f722900e87c19d1e970
raonytog <togneriraony@gmail.com>
2023-08-28 16:52:35 -0300

    resolvido 07

---
+++ 02_recursao/rec_07/rec7.c
@@ -0,0 +1,20 @@
+/*
+
+
+*/
+
+#include <stdio.h>
+
+int somaDigitos(int n);
+
+int main () {
+    int num;
+    scanf("%d", &num);
+    printf("%d", somaDigitos(num));
+    return 0;
+}
+
+int somaDigitos(int n) {
+    if (n < 10) return n;
+    return n%10 + somaDigitos(n/10);
+}
