cfc67fcd00331cf15d19ae05cc12cdb89eff532d
raonytog <togneriraony@gmail.com>
2023-08-27 21:16:54 -0300

    rec04 resolvido

--- 02_recursao/rec_04/rec4.c
+++ 02_recursao/rec_04/rec4.c
@@ -1,24 +1,31 @@
 /*
-
-
-
+Programa que recebe palavras enquanto o usuário digitar e retorna se elas são ou não palindromas 
+(i.e. se lidas da direita a esquerda, continuam a mesma palavra lida da esquerda a direita).
+Raony Togneri
+27/08/2023
 */
 
 #include <stdio.h>
 #include <string.h>
 #define MAX 1001
 
-int palindromo(char *string, int tamanho); 
+int EhPalindromo(char *string, int tamanho); 
 
 int main () {
     char palavra[MAX];
-    scanf("%s", palavra);
-    int tam = strlen(palavra);
-    printf("%d", tam);
+    while (scanf("%s", palavra) == 1) {
+        int tam = strlen(palavra);
+        if (EhPalindromo(palavra, tam)) printf("SIM\n");
+        else printf("NAO\n");
+    }
 
     return 0;
 }
 
-int palindromo(char *string, int tamanho) {
-    return 1;
+int EhPalindromo(char *string, int tamanho) {
+    if (tamanho <= 1) return 1;
+    
+    if (string[0] != string[tamanho-1]) return 0;
+
+    return EhPalindromo(string+1, tamanho-2);
 }
