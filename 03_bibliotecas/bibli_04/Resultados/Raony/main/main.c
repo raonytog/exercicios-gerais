8176d95e8c2f0f5b75ea357fef1d5b06c07096ab
raonytog <raony.gomes@edu.ufes.br>
2023-09-04 18:15:13 -0300

    media 10

---
+++ 03_bibliotecas/bibli_04/Resultados/Raony/main/main.c
@@ -0,0 +1,30 @@
+/*
+Programa criado para manipular as datas com a biblioteca criada
+Raony Togneri
+02/09/2023
+*/
+
+#include <stdio.h>
+#include "data.h"
+
+int main () {
+    int dia, mes, ano;
+    scanf("%02d/%02d/%04d%*c", &dia, &mes, &ano);
+
+    if (verificaDataValida(dia, mes, ano)) {
+        printf("Data informada: ");
+        imprimeDataExtenso(dia, mes, ano);
+
+        if (verificaBissexto(ano)) printf("O ano informado eh bissexto\n");
+        else printf("O ano informado nao eh bissexto\n");
+
+        printf("O mes informado possui %d dias\n", numeroDiasMes(mes, ano));
+
+        printf("A data seguinte eh: ");
+        imprimeProximaData(dia, mes, ano);
+        printf("\n");
+
+    } else printf("A data informada eh invalida\n");
+    
+    return 0;
+}
