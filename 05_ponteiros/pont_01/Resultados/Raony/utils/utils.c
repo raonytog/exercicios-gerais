99de3dee20bee9b2284a93b225617102da9cdeb7
raonytog <raony.gomes@edu.ufes.br>
2023-09-25 22:45:34 -0300

    feito

---
+++ 05_ponteiros/pont_01/Resultados/Raony/utils/utils.c
@@ -0,0 +1,14 @@
+#include "utils.h"
+
+int TrocaInterios(int * n1, int * n2) {
+    if (*n1 > *n2) {
+        int temp = 0;
+        temp = *n1;
+        *n1 = *n2;
+        *n2 = temp;
+        return 1;
+    } 
+
+    return 0;
+}
+
